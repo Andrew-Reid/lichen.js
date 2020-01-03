12f62d7d5ea1007ff37e9c64f60fc71e6f1f169b
Andrew-Reid <reidandy@hotmail.com>
2020-01-02 23:01:28 -0800

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,44 @@
+
+import ptSquare from './ptSquare.js';
+import ptCircle from './ptCircle.js';
+import ptHexagon from './ptHexagon.js';
+import ptLetter from './ptLetter.js';
+import ptSymbol from './ptSymbol.js';
+import ptChecker from './ptChecker.js';
+import ptStripe from './ptStripe.js';
+import ptPlaid from './ptPlaid.js';
+import ptCairo from './ptCairo.js';
+import ptOctagon from './ptOctagon.js';
+import ptSine from './ptSine.js';
+
+import ptSchemeCategory10 from './schemes/ptSchemeCategory10.js';
+import ptCircles from './schemes/ptCircles.js';
+
+import ptManager from './ptStripeManager.js';
+
+import {ptWetland, ptFish, ptFire, ptDanger} from './symbols/ptSymbols.js';
+
+
+
+export {
+  ptSquare as ptSquare,
+  ptCircle as ptCircle,
+  ptHexagon as ptHexagon,
+  ptLetter as ptLetter,
+  ptSymbol as ptSymbol,
+  ptWetland as ptWetland,
+  ptFish as ptFish,
+  ptFire as ptFire,
+  ptDanger as ptDanger,
+  ptChecker as ptChecker,
+  ptStripe as ptStripe,
+  ptPlaid as ptPlaid,
+  ptCairo as ptCairo,
+  ptOctagon as ptOctagon,
+  ptSine as ptSine,
+  
+  ptSchemeCategory10 as ptSchemeCategory10,
+  ptCircles as ptCircles,
+  ptManager as ptManager
+  
+}
